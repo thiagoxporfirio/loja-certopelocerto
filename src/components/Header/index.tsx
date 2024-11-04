d797cafb8055a86fd039b2dfb8fd63b204966589
thiagoxporfirio <thiagovporfirio@gmail.com>
2024-11-04 20:55:45 -0300

    fix: component and position links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,53 +10,60 @@ const Header: React.FC = () => {
 
 	return (
 		<header className="relative flex justify-between items-center p-8 border-b border-t">
-			<nav className="flex space-x-6">
+			<nav className="flex items-center space-x-8">
 				<CustomLink to="/new" text="NEW" />
 				<div
-					className="relative"
+					className="relative flex items-center"
 					onMouseEnter={handleMouseEnter}
 					onMouseLeave={handleMouseLeave}
 				>
 					<CustomLink to="/shop" text="SHOP" />
 					{isDropdownOpen && (
-						<div className="absolute top-full left-0 mt-2 w-64 bg-gray-100 p-4 shadow-lg">
-							<h4 className="font-bold mb-2">COLLECTIONS</h4>
-							<ul className="space-y-1">
-								<li>
-									<Link to="/collections/drifting">Drifting Machines</Link>
-								</li>
-								<li>
-									<Link to="/collections/daily">No Half Sends | Daily 24</Link>
-								</li>
-								<li>
-									<Link to="/collections/flatout">Flat Out</Link>
-								</li>
-								<li>
-									<Link to="/collections/endless">Endless Sands</Link>
-								</li>
-								<li>
-									<Link to="/collections/attitude">Full Sends Attitude</Link>
-								</li>
-								<li>
-									<Link to="/collections/motorverse">Motorverse</Link>
-								</li>
-								<li>
-									<Link to="/collections/racing">Racing Community</Link>
-								</li>
-								<li>
-									<Link to="/collections/legends">Legends</Link>
-								</li>
-							</ul>
+						<div
+							className="absolute left-0 top-full mt-0 w-screen bg-gray-100 py-8 shadow-lg z-10"
+							style={{ paddingLeft: "2rem", paddingRight: "2rem" }}
+						>
+							<div className="max-w-screen-xl mx-auto px-4">
+								<h4 className="font-bold mb-4">COLLECTIONS</h4>
+								<ul className="space-y-1">
+									<li>
+										<Link to="/collections/drifting">Drifting Machines</Link>
+									</li>
+									<li>
+										<Link to="/collections/daily">
+											No Half Sends | Daily 24
+										</Link>
+									</li>
+									<li>
+										<Link to="/collections/flatout">Flat Out</Link>
+									</li>
+									<li>
+										<Link to="/collections/endless">Endless Sands</Link>
+									</li>
+									<li>
+										<Link to="/collections/attitude">Full Sends Attitude</Link>
+									</li>
+									<li>
+										<Link to="/collections/motorverse">Motorverse</Link>
+									</li>
+									<li>
+										<Link to="/collections/racing">Racing Community</Link>
+									</li>
+									<li>
+										<Link to="/collections/legends">Legends</Link>
+									</li>
+								</ul>
+							</div>
 						</div>
 					)}
 				</div>
 			</nav>
 
-			<div>
+			<div className="flex items-center">
 				<img src={logo} alt="Logo" className="h-16" />
 			</div>
 
-			<nav className="flex space-x-6">
+			<nav className="flex items-center space-x-8">
 				<CustomLink to="/account" text="CONTA" />
 				<CustomLink to="/cart" text="CARRINHO" />
 			</nav>
@@ -70,7 +77,10 @@ interface CustomLinkProps {
 }
 
 const CustomLink: React.FC<CustomLinkProps> = ({ to, text }) => (
-	<Link to={to} className="relative text-sm font-semibold group">
+	<Link
+		to={to}
+		className="relative text-sm font-semibold group flex items-center"
+	>
 		{text}
 		<span className="block h-[2px] bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out origin-left"></span>
 	</Link>
