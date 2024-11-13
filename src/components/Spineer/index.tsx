9e5b02dc83cb29d7ac123d87394f7b48cef583c0
thiagoxporfirio <thiagovporfirio@gmail.com>
2024-11-13 11:50:06 -0300

    install mui and spineer

--- src/components/Spineer/index.tsx
+++ src/components/Spineer/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { CircularProgress } from "@mui/material";
 
-const Spineer: React.FC = () => {
+const Spinner: React.FC = () => {
 	return (
 		<div className="min-h-screen flex items-center justify-center">
-			<div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-gray-900"></div>
+			<CircularProgress
+				size={64}
+				thickness={4}
+				disableShrink
+				variant="indeterminate"
+				sx={{
+					color: "black"
+				}}
+			/>
 		</div>
 	);
 };
 
-export default Spineer;
+export default Spinner;
